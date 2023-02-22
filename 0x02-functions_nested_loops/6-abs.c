899974eb7e346153f2554befddba326480b22391
Sadiq Aliyu <sadiiq.aliiyu@gmail.com>
2023-02-22 04:53:03 +0000

    add commit for c function up to exercise 8

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @1: input number as an integer
+ *
+ * Return; absolute value
+ */
+int _abs(int 1)
+{
+	if (i >= 0)
+	{
+		return(i);
+	}
+	else
+	{
+		return(i * -1);
+	}
+}
