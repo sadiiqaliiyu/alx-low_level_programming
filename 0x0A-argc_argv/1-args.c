2696c66e6807f704d3f6475bc0c36ba727c68b68
Sadiq Aliyu <sadiiq.aliiyu@gmail.com>
2023-08-13 17:34:12 +0000

    add 1.args.c task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main-prints out number of argument
+ * @argc: number of arguments
+ * @argv:array strings argument
+ * Return:always 0 (success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
