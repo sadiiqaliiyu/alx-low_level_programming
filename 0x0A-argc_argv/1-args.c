52d8fffb5999eb6d3c207cba6ac6f025d9a50739
Sadiq Aliyu <sadiiq.aliiyu@gmail.com>
2023-08-13 17:43:23 +0000

    added attribute for unused arg

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  *
  * Return: always 0 (success)
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
