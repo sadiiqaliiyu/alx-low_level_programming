0c44b290c6384e539a8accc31fcb65a11b58a11f
Sadiq Aliyu <sadiiq.aliiyu@gmail.com>
2023-08-13 17:38:00 +0000

    made some more correction

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * main-prints out number of argument
+ * main - prints out number of argument
  * @argc: number of arguments
- * @argv:array strings argument
- * Return:always 0 (success)
+ * @argv: array strings argument
+ *
+ * Return: always 0 (success)
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
