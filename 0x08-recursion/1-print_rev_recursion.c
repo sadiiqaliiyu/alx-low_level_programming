946812a72fce95025211ff1465b275b8af914cc0
Sadiq Aliyu <sadiiq.aliiyu@gmail.com>
2023-08-26 22:50:32 +0000

    fix another betty error

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,21 +1,13 @@
-#include <stdio.h>
 #include "main.h"
-#include <string.h>
 /**
  *_print_rev_recursion - print text in reverse
  *@s: string argument
  */
-
 void _print_rev_recursion(char *s)
 {
-	int i;
-	char *string = s;
-	char revString[6];
-	int j;
-
-	for (i = strlen(string) - 1; i >= 0; i--)
+	if (*s)
 	{
-		revString[j++] = string[i];
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	printf("%s\n", revString);
 }
