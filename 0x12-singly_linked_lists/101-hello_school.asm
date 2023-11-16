31796429061c55c5b2d414300e096c8f6a1ab37d
Sadiq Aliyu <sadiiq.aliiyu@gmail.com>
2023-11-16 21:56:15 +0000

    add some corrections

--- 0x12-singly_linked_lists/101-hello_school.asm
+++ 0x12-singly_linked_lists/101-hello_school.asm
@@ -1,15 +1,21 @@
-	SECTION .data
-msg:	db "Hello, Holberton", 0
-fmt:	db "%s", 10, 0
 
-	SECTION .text
-	extern printf
-	global main
+		extern printf
+
+		SECTION .data
+message:    	db "Hello, Holberton", 0
+format:    	db "%s", 10, 0
+		SECTION .text
+
+		global main
 main:
-	mov esi, msg
-	mov edi, fmt
-	mov eax, 0
-	call printf
 
-	mov eax, 0
-	ret
+		push rbp
+		mov rdi,format
+		mov rsi,message
+		mov rax,0
+		call printf
+
+		pop rbp
+
+		mov rax,0
+		ret
